25ef4f66bb4cf10c5c50c5d9bd02bd13a1d16483
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-09-10 11:27:19 -0400

    added dependencies, added deploy contract, added token.sol

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const Token = artifacts.require("Token");
+
+
+module.exports = async function (deployer) {
+    const accounts = await web3.eth.getAccounts()
+
+    await deployer.deploy(Token);
+   
+};
